3044020bf09d8f224dacf12ee5c7371c1446f602
rayan2228 <rhrayan2228@gmail.com>
2023-12-13 19:40:26 +0600

    components ready

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import RootLayout from "./components/layouts/RootLayout";
 import Home from "./pages/Home";
+import SingleBlog from "./pages/SingleBlog";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="/blog/:blogid" element={<SingleBlog />} />
     </Route>
   )
 );
---
+++ src/components/singleBlog/SingleBlogContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleBlogContainer = () => {
+  return <div>SingleBlogContainer</div>;
+};
+
+export default SingleBlogContainer;
