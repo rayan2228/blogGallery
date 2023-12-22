85b4b596ca48b6448058b5852704978c01f6ca2d
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-12-22 22:00:25 +0600

    form added

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -6,23 +6,26 @@ const Search = () => {
   const searchValue = useSelector((state) => state.filterBlog.search);
   const [search, setSearch] = useState(searchValue);
   const dispatch = useDispatch();
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.target.preventDefault();
     dispatch(searchBlog(search));
   };
   return (
     <div className="justify-self-end">
-      <input
-        type="search"
-        className="px-5 py-3 text-xl border border-green-950 outline-transparent"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button
-        onClick={handleSubmit}
-        className="w-32 px-5 py-3 text-xl font-semibold text-white border-2 rounded-tr-lg rounded-br-lg bg-green-950 border-green-950"
-      >
-        search
-      </button>
+      <form>
+        <input
+          type="search"
+          className="px-5 py-3 text-xl border border-green-950 outline-transparent"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          onClick={handleSubmit}
+          className="w-32 px-5 py-3 text-xl font-semibold text-white border-2 rounded-tr-lg rounded-br-lg bg-green-950 border-green-950"
+        >
+          search
+        </button>
+      </form>
     </div>
   );
 };
